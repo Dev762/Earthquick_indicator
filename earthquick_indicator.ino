4bb1855b5d95de49e111b305a1098d2abf289573
Devendra Singh Shekhawat <shekhawat.dev313@gmail.com>
2017-11-28 11:00:55 -0800

    Update earthquick_indicator.ino

--- earthquick_indicator.ino
+++ earthquick_indicator.ino
@@ -1,27 +1,7 @@
-
-/*
- ADXL3xx
-
- Reads an Analog Devices ADXL3xx accelerometer and communicates the
- acceleration to the computer.  The pins used are designed to be easily
- compatible with the breakout boards from Sparkfun, available from:
- http://www.sparkfun.com/commerce/categories.php?c=80
-
- http://www.arduino.cc/en/Tutorial/ADXL3xx
-
- The circuit:
-  analog 2: z-axis
- analog 1: y-axis
- analog 0: x-axis
-
- LED pin 13
- Buzzer pin 7
-*/
-
 // these constants describe the pins. They won't change:
-const int xpin = A1;                  // x-axis of the accelerometer
-const int ypin = A2;                  // y-axis
-const int zpin = A3;                  // z-axis (only on 3-axis models)
+const int xpin = A0;                  // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
 
 #define led 13
 #define buzzer 7
@@ -56,37 +36,37 @@ void loop() {
   {
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
     delay(100);
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
     delay(100);
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
     delay(100);
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
     delay(100);
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
     delay(100);
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    delay(500);
+    delay(400);
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
   }
